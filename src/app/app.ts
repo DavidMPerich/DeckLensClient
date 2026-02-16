d8128bfcfcf6be0f8d3a2318fa1df6446268535d
DavidMPerich <davidmperich@gmail.com>
2026-02-16 12:54:17 -0700

    add successful test request to api

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -7,6 +8,16 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected title = 'client';
+export class App  implements OnInit {
+  private http = inject(HttpClient);
+  protected title = 'Deck Lens';
+  protected card = signal<any>([]);
+
+  ngOnInit(): void {
+    this.http.get("https://localhost:7138/api/cards/Annul").subscribe({
+      next: response => this.card.set(response),
+      error: error => console.log(error),
+      complete: () => console.log("Request complete")
+    });
+  }
 }
