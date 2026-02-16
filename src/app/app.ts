04250a7a4c3c59946addd66e3b36383aa2fe4e26
DavidMPerich <davidmperich@gmail.com>
2026-02-16 15:27:47 -0700

    create stub nav bar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Nav } from "../layout/nav/nav";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Nav],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
