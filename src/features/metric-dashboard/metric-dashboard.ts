8d2e7f379b805213ef779394822c99e09a4ab1a6
DavidMPerich <davidmperich@gmail.com>
2026-03-02 14:18:54 -0700

    add navigation to metric panel

--- src/features/metric-dashboard/metric-dashboard.ts
+++ src/features/metric-dashboard/metric-dashboard.ts
@@ -1,9 +1,10 @@
-import { Component, ElementRef, input, Input, ViewChild } from '@angular/core';
+import { Component, ElementRef, inject, input, Input, ViewChild } from '@angular/core';
 import { ManaCurveChart } from '../metrics/mana-curve-chart/mana-curve-chart';
 import { ColorDistributionChart } from '../metrics/color-distribution-chart/color-distribution-chart';
 import { CardTypeBreakdownChart } from '../metrics/card-type-breakdown-chart/card-type-breakdown-chart';
 import { DeckAnalysisDto } from '../../types/deck';
 import { MetricPanel } from "../metric-panel/metric-panel";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-metric-dashboard',
@@ -16,10 +17,14 @@ import { MetricPanel } from "../metric-panel/metric-panel";
   styleUrl: './metric-dashboard.css',
 })
 export class MetricDashboard {
+  private router = inject(Router);
   analysis = input.required<DeckAnalysisDto>();
   showManaCurve = input(false);
-
   @ViewChild('clink') clink!: ElementRef<HTMLAudioElement>;
+
+  navigate() {
+    this.router.navigate(['/']);
+  }
 
   playHoverSound() {
     const audio = this.clink.nativeElement;
